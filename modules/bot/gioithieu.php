e68f60c629ad21e8dff8a3f57d06002a462c4389
TjeuBao <1451010013bao@ou.edu.vn>
2017-10-19 03:22:57 +0700

    Add files via upload

---
+++ modules/bot/gioithieu.php
@@ -0,0 +1,145 @@
+<style>
+.main {
+    min-height: 450px;
+}
+.main {
+    margin-bottom: 10px;
+}
+.container, .container-fluid {
+    padding-left: 10px;
+    padding-right: 10px;
+}
+
+@media (min-width: 1200px)
+.container {
+    width: 1170px;
+}
+@media (min-width: 992px)
+.container {
+    width: 970px;
+}
+@media (min-width: 768px)
+.container {
+    width: 750px;
+}
+.container {
+    padding-right: 15px;
+    padding-left: 15px;
+    margin-right: auto;
+    margin-left: auto;
+}
+.row {
+    margin-left: -10px;
+    margin-right: -10px;
+}
+.menu-about h3{
+    border-bottom: 2px solid #006fc0;
+    text-transform: uppercase;
+    margin-top: 0;
+}
+h4 {
+    font-size: 30px;
+}
+.menu-about h4 span {
+    background: #006fc0 none repeat scroll 0 0;
+    color: #fff;
+    display: inline-block;
+    font-size: 17px;
+    line-height: 40px;
+    margin-left: 464px;  
+    width: 223px;
+    padding: 0 40px 0 34px;
+    position: relative;
+    text-transform: uppercase;
+    border-radius: 2px;
+    padding: 2px;
+}
+.content{
+	margin-top: 74px;
+    margin-left: -281px;
+}
+</style>
+
+<div class="main">
+        <div class="container">
+            <div class="row">
+                    <div class="col-md-3">
+
+<div class="menu-about">
+    <h4>
+        <span>
+            Giới thiệu
+        </span>
+    </h4>
+   
+</div>                    </div>
+                    <div class="col-md-9">
+
+<div class="breadcrumb clearfix">
+</div>
+<script type="text/javascript">
+    $(".link-site-more").hover(function () { $(this).find(".s-c-n").show(); }, function () { $(this).find(".s-c-n").hide(); });
+</script>
+<div class="about-detail">
+    <div class="about-block">
+      
+        <div class="content">
+            <div>
+	Giới thiệu về Shop Giày</div>
+<div>
+	Ra đời vào cuối năm 2017, Shop Giày đã sớm có một vị trí vững chắc trên thị trường thương mại điện tử trong nước, trở thành địa chỉ mua sắm online đầy tin cậy, tiện lợi cho khách hàng tại TP.HCM và các tỉnh thành khác trên toàn quốc.</div>
+<div>
+	&nbsp;</div>
+<div>
+	Shop Giày &nbsp;là một website thời trang online, phục vụ nhu cầu mua sắm trực tuyến bao gồm: chọn và mua hàng trực tuyến nhanh chóng, thanh toán trực tuyến an toàn, giao hàng tận nơi, chăm sóc và tư vấn thân thiện. Với mong muốn đem đến cho khách hàng những sản phẩm chất lượng,thời trang với giá cả hơp lý nhất, mang ứng dụng cao, hiện đại, năng động, phù hợp với văn hóa, gu thẩm mỹ của người Việt Nam.</div>
+<div>
+	&nbsp;</div>
+<div>
+	Tầm nhìn:</div>
+<div>
+	Shop Giày mong muốn trở thành website cung cấp những sản phẩm thời trang uy tín, chất lượng và tốt nhất cho quý khách hàng. Đưa website Shop Giày trở thành nơi mua sắm trực tuyến ưa thích và thường xuyên của khách hàng.</div>
+<div>
+	&nbsp;</div>
+<div>
+	Sứ mệnh:</div>
+<div>
+	Sản phẩm trên Shop Giày &nbsp;là những sản phẩm của thời trang hiện đại &nbsp;mang tính hữu dụng để mọi người có thể tự do kết hợp và sáng tạo chúng trong phong cách độc đáo riêng của từng người mà không phân biệt tuổi tác và ngành nghề. Mỗi sản phẩm được đăng trên website Shop Giày đều được tuyển chọn và bảo đảm chất lượng, và chúng tôi hi vọng đó cũng sẽ là món quà ý nghĩa để bạn giành cho những người thân yêu nhất.</div>
+<div>
+	&nbsp;</div>
+<div>
+	Sản phẩm chủ đạo của chúng tôi là:</div>
+<div>
+	- Các sản phẩm thời trang nam nữ, bao gồm: Giày Vans, Nike, Converse,... với mẫu mã đa dạng, đảm bảo phù hợp với thị hiếu thời trang và túi tiền của quý khách.</div>
+<div>
+	&nbsp;</div>
+<div>
+	Các sản phẩm của chúng tôi đều được nhập từ các nhà cung cấp thời trang uy tín,làm từ các chất liệu tốt, đáp ứng đầy đủ các yêu cầu về thẩm mỹ và chất lượng sản phẩm. Ngoài ra Shop Giày luôn cố gắng khai thác thêm nhiều mẫu mã, đảm bảo được sự đa dạng và độc đáo của mỗi sản phẩm khi xuất hiện trên website Shop Giày Chúng tôi sẽ luôn đáp ứng nhằm phục vụ các Quý khách một cách tốt nhất.</div>
+<div>
+	&nbsp;</div>
+<div>
+	Đặt mục tiêu trở thành một trung tâm mua sắm trực tuyến hàng đầu tại Việt Nam về các sản phẩm dẫn đầu về uy tín, chất lượng và dịch vụ, Shop Giày cam kết:</div>
+<div>
+	&nbsp;</div>
+<div>
+	- Cung cấp các sản phẩm - dịch vụ chất lượng tốt, giá cả phù hợp với túi tiền người tiêu dùng.</div>
+<div>
+	- Các sản phẩm được kiểm tra, sàng lọc kỹ càng, đảm bảo an toàn cho sức khỏe cho người sử dụng.</div>
+<div>
+	- Bảo hành nghiêm túc theo quy định của nhà sản xuất.</div>
+<div>
+	- Được đổi, trả hàng trong vòng 15 ngày hoàn toàn miễn phí.</div>
+<div>
+	- Tư vấn, hướng dẫn sử dụng với từng sản phẩm, đặc biệt với các sản phẩm chuyên dụng.</div>
+<div>
+	&nbsp;</div>
+<div>
+	Với các tiêu chí về chất lượng và một đội ngũ năng động, nhiệt tình, giàu kinh nghiệm, Shop Giày nhất định sẽ là điểm mua sắm lý tưởng cho các đối tượng khách hàng trên cả nước, nhanh gọn, an toàn và tiết kiệm thời gian hiệu quả.</div>
+
+        </div>
+        <div class="social-group">
+        </div>
+    </div>
+</div>                    </div>
+            </div>
+        </div>
+    </div>
